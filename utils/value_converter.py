c81729bac03549a424ad60b81c7aa66772d82632
MateWagner <wwagnermate@gmail.com>
2023-08-09 07:55:03 +0000

    refactor: logic moved to utils from mqtt_bridge

---
+++ utils/value_converter.py
@@ -0,0 +1,39 @@
+import re
+import utils.config_parser as config
+from utils.data_types import ColorRgbw
+
+
+def get_rgbw(hex_rgb):
+    red = 0
+    green = 0
+    blue = 0
+    white = 0
+
+    if hex_rgb == '#FFFFFF' and 'W' in config.ORDER.upper():
+        white = 255
+    else:
+        red = hex_to_rgb(hex_rgb)[0]
+        green = hex_to_rgb(hex_rgb)[1]
+        blue = hex_to_rgb(hex_rgb)[2]
+
+    return ColorRgbw(red, green, blue, white)
+
+
+def hex_to_rgb(hx, hsl=False):
+    """Converts a HEX code into RGB or HSL.
+    Args:
+        hx (str): Takes both short as well as long HEX codes.
+        hsl (bool): Converts the given HEX code into HSL value if True.
+    Return:
+        Tuple of length 3 consisting of either int or float values.
+    Raise:
+        ValueError: If given value is not a valid HEX code."""
+    # Thank you to: https://stackoverflow.com/questions/29643352/converting-hex-to-rgb-value-in-python
+    if re.compile(r'#[a-fA-F0-9]{3}(?:[a-fA-F0-9]{3})?$').match(hx):
+        div = 255.0 if hsl else 0
+        if len(hx) <= 4:
+            return tuple(int(hx[i]*2, 16) / div if div else
+                         int(hx[i]*2, 16) for i in (1, 2, 3))
+        return tuple(int(hx[i:i+2], 16) / div if div else
+                     int(hx[i:i+2], 16) for i in (1, 3, 5))
+    raise ValueError(f'"{hx}" is not a valid HEX code.')
