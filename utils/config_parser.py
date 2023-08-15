b127d9458b134b852e7b33ac8801966ecff7db12
MateWagner <wwagnermate@gmail.com>
2023-08-15 13:22:57 +0200

    feat: add concreet path for config file

--- utils/config_parser.py
+++ utils/config_parser.py
@@ -1,7 +1,18 @@
 import os
 import yaml
-conf = yaml.safe_load(open("config.yml"))
-# print(conf['NEOPIXEL']['PIN'])
+
+def parse_config_yml():
+  # Define the relative path to the config.yml file
+  CONFIG_FILE_RELATIVE_PATH = "../config.yml"
+
+  # Get the absolute path of the directory containing this script
+  script_directory = os.path.dirname(os.path.abspath(__file__))
+
+  # Load the configuration from the config.yml file
+  return  yaml.safe_load(open(os.path.join(script_directory, CONFIG_FILE_RELATIVE_PATH)))
+
+
+conf = parse_config_yml()
 # neopixel constans
 NUM_PIXEL = conf['NEOPIXEL']['NUM_PIXEL']
 ORDER = conf['NEOPIXEL']['ORDER']
