108bfe863a08cd5138780d8a1a11b750cd5e785d
MateWagner <wwagnermate@gmail.com>
2023-11-20 14:18:58 +0100

    style: naming more specific

--- utils/enums.py
+++ utils/enums.py
@@ -8,9 +8,19 @@ default_color = ColorRgbw(255, 125, 255,)
 
 
 class ShowType(Enum):
-    OFF = auto()
+    COLOR = "Color"
+    RAINBOW_LOOP = "Rainbow Loop"
+    COLOR_LOOP = "Color Loop"
+
+    @classmethod
+    def get_show_type_dict(cls):
+        return {member.name: member.value for member in cls}
+
+
+class RenderType(Enum):
     COLOR = auto()
     RAINBOW = auto()
+    COLOR_LOOP = auto()
 
 
 class CycleState(Enum):
